cf1a96dc71c2e846c1b16d68ed028ee33912fc3d
jtontiwith <jtonti@gmail.com>
2018-09-05 15:17:50 -0500

    redeploy after playing with env vars again

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||
 "http://localhost:8080";
 
 
+
 //just setting the default values for the project
 //heroku is going to use the process.env....
